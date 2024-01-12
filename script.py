548e330a614d74ef67f4318c88436c856d845f60
Rindirisia Wangira <rindirisiawangira@gmail.com>
2024-01-11 21:49:59 -0600

    Reverting back

--- script.py
+++ script.py
@@ -18,7 +18,7 @@ if __name__ == '__main__':
 			if (run_guess(guess, answer)):
 				break
 		except ValueError:
-			print('PLEASE enter a number')
+			print('Please enter a number')
 			continue
 
 
